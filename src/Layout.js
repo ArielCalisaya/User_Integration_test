a110ee59e0e4649e6e3d00b02ffe35df46f78eaf
Ariel Calisaya <calisayaariel62@gmail.com>
2019-12-07 02:12:21 -0300

    added Semantic-ui

--- src/Layout.js
+++ src/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './views/home';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+
 function Layout() {
   return (
       <BrowserRouter>
--- src/views/home.js
+++ src/views/home.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Button, Divider, Form } from 'semantic-ui-react';
 
 export default class Home extends React.Component {
     render(){
         return(
-            <div className="">
+            <div className="Bg-Home">
                 <div className="text-center">
                     <h1>
                         <span>Hello World</span>
                     </h1>
+                </div>
+                <div className="formContent">
+                    <Form size='large'>
+                      <Form.Group widths='equal'>
+                        <Form.Input
+                          width={6}
+                          label='First name'
+                          placeholder='First name'
+                        />
+                        <Form.Input
+                          width={6}
+                          label='Last name'
+                          placeholder='Last name'
+                        />
+                      </Form.Group>
+                      <Button type='submit'>Submit</Button>
+                      <Divider hidden />
+                    </Form>
                 </div>
 
             </div>
